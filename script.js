a8e7715c704aeac8fac61e901278659e19bd3c65
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-27 18:19:04 +0400

    feat: add textarea

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ const keyKeyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit
 
 const codeKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-function initWrapper(){
-  let out = '<section class="keyboard-wrapper"></section>';
+
+
+// function initTextareaWrapper(){
+//   let out = '<section class="textarea-wrapper"></section>';
+//   document.querySelector('.keyboard').innerHTML = out;
+// }
+
+// initTextareaWrapper();
+
+function initKeyboardWrapper(){
+  let out = '<section class="keyboard-wrapper"><textarea></textarea></section>';
   document.querySelector('.keyboard').innerHTML = out;
 }
 
-initWrapper();
+initKeyboardWrapper();
 
 
 function initBoard(){
   let out = '';
+  out = '<textarea class="textarea" rows="10" cols="100"></textarea>';
   for (let i = 0; i < codeKeyboard.length; i++){
     if (codeKeyboard[i] == 'Enter' || codeKeyboard[i] == 'Backspace' || codeKeyboard[i] == 'Tab' || codeKeyboard[i] == 'CapsLock'|| codeKeyboard[i] == 'Shift' || codeKeyboard[i] == 'Control' || codeKeyboard[i] == 'Alt'){
       out += '<div class="k-key specical-key" data="'+keyKeyboard[i]+'">'+codeKeyboard[i]+'</div>'
