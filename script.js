5b293418b7d881aaf3bec02c0563ff54b2167281
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-27 19:45:12 +0400

    feat: add animation on click

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const keyKeyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit
 
 const codeKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-
+// const area = document.querySelector('.textarea');
 
 // function initTextareaWrapper(){
 //   let out = '<section class="textarea-wrapper"></section>';
@@ -21,7 +21,7 @@ initKeyboardWrapper();
 
 function initBoard(){
   let out = '';
-  out = '<textarea class="textarea" rows="10" cols="100"></textarea>';
+  out = '<textarea class="textarea" rows="10" cols="130"></textarea>';
   for (let i = 0; i < codeKeyboard.length; i++){
     if (codeKeyboard[i] == 'Enter' || codeKeyboard[i] == 'Backspace' || codeKeyboard[i] == 'Tab' || codeKeyboard[i] == 'CapsLock'|| codeKeyboard[i] == 'Shift' || codeKeyboard[i] == 'Control' || codeKeyboard[i] == 'Alt'){
       out += '<div class="k-key specical-key" data="'+keyKeyboard[i]+'">'+codeKeyboard[i]+'</div>'
@@ -45,3 +45,27 @@ document.onkeydown = function(event){
 document.onkeyup = function (event){
   document.querySelector('.k-key[data="'+ event.code +'"]').classList.remove('active');
 }
+
+const keys = document.querySelectorAll('.k-key');
+
+for (let key of keys){
+  key.addEventListener('mousedown', function(){
+    key.classList.add('active');
+  })
+  key.addEventListener('mouseup', function(){
+    key.classList.remove('active');
+  })
+}
+
+console.log(keys)
+
+function getDataKeyPress(event){
+  console.log(event)
+}
+
+keys.forEach(key => key.addEventListener('keydown', getDataKeyPress))
+
+
+
+
+
