a1478c5fcaae3630240fae81b735b2dffbb593a5
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-30 22:55:27 +0400

    fix: correct upper arr key-numbers

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const keyKeyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit
 
 const codeKeyboardLowerEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardUpperEng = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
+const codeKeyboardUpperEng = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
 const codeKeyboardLowerRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardUpperRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
+const codeKeyboardUpperRu = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
 let currentArrBoard = codeKeyboardLowerEng;
 
