5215de66e84050e5d6873d597798d3b4a3efe277
Denis Svetleishii <denissvetleishii@outlook.com>
2023-04-30 17:42:43 +0400

    feat: add arrow input

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const keyKeyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-const codeKeyboardLowerEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const codeKeyboardLowerEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardUpperEng = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const codeKeyboardUpperEng = ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardLowerRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const codeKeyboardLowerRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardUpperRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const codeKeyboardUpperRu = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
 let currentArrBoard = codeKeyboardLowerEng;
 
@@ -32,7 +32,10 @@ function initBoard(){
   textareaInit.innerHTML = '';
   let out = '';
   for (let i = 0; i < currentArrBoard.length; i++){
-    if (currentArrBoard[i] == 'Alt'){
+    if (currentArrBoard[i] == '&#x2191;'|| currentArrBoard[i] == '&#x2190;' || currentArrBoard[i] == '&#x2193;' || currentArrBoard[i] == '&#x2192;'){
+      out += '<div class="k-key arrow-key" data="'+keyKeyboard[i]+'">'+currentArrBoard[i]+'</div>';
+    }
+    else if (currentArrBoard[i] == 'Alt'){
       out += '<div class="k-key specical-key alt-key" data="'+keyKeyboard[i]+'">'+currentArrBoard[i]+'</div>';
     }
     else if (currentArrBoard[i] == 'Enter'){
@@ -78,6 +81,7 @@ document.onkeydown = function(event){
   let target = event.code;
   let positionKey = keyKeyboard.indexOf(target);
   let pressSymbol = currentArrBoard[positionKey];
+  console.log(pressSymbol)
 
   if (pressSymbol == 'Backspace'){
     let backspaceArr = area.value.split('');
@@ -89,6 +93,26 @@ document.onkeydown = function(event){
   }
   else if (pressSymbol == 'Enter'){
     area.value += '\n';
+  }
+  else if (pressSymbol == '&#x2191;'){
+    arrow = document.createElement('span');
+    arrow.innerHTML = '&#x2191;';
+    area.value += arrow.innerHTML;
+  }
+  else if (pressSymbol == '&#x2190;'){
+    arrow = document.createElement('span');
+    arrow.innerHTML = '&#x2190;';
+    area.value += arrow.innerHTML;
+  }
+  else if (pressSymbol == '&#x2193;'){
+    arrow = document.createElement('span');
+    arrow.innerHTML = '&#x2193;';
+    area.value += arrow.innerHTML;
+  }
+  else if (pressSymbol == '&#x2192;'){
+    arrow = document.createElement('span');
+    arrow.innerHTML = '&#x2192;';
+    area.value += arrow.innerHTML;
   }
   else if (pressSymbol == 'CapsLock'){
     switch (currentArrBoard){
