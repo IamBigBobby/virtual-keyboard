2936e7759b668d08d4fc673f2d2180fe59fa1471
Denis Svetleishii <denissvetleishii@outlook.com>
2023-05-01 21:48:13 +0400

    fix: add symbols to upper eng arr

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const keyKeyboard = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit
 
 const codeKeyboardLowerEng = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
-const codeKeyboardUpperEng = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
+const codeKeyboardUpperEng = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', "\"", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
 const codeKeyboardLowerRu = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#x2191;', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', '&#x2190;', '&#x2193;', '&#x2192;'];
 
